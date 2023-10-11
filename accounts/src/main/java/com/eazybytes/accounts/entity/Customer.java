7a62a364b24763342b220786245b04533654099c
Rahul Vijay <105475681+RahulVijay17@users.noreply.github.com>
2023-10-12 00:35:38 +0530

    Added Security

---
+++ accounts/src/main/java/com/eazybytes/accounts/entity/Customer.java
@@ -0,0 +1,21 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Customer extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO,generator = "native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private Long customerId;
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
